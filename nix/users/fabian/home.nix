c544565ef9faf411822934efa21d50d7d5555015
schMarXman <asdf@test.com>
2024-10-01 22:56:33 +0200

    Move more gnome settings

--- nix/users/fabian/home.nix
+++ nix/users/fabian/home.nix
@@ -87,29 +87,6 @@
     lg = "lazygit";
   };
 
-  # Gnome settings
-  dconf.settings = {
-    # Window buttons
-    "org/gnome/desktop/wm/preferences" = {
-      button-layout = "appmenu:minimize,close";
-    };
-
-    # Keybindings
-    "org/gnome/desktop/wm/keybindings" = {
-      minimize = ["<Super>x"];
-    };
-  
-    "org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = [
-        "appindicatorsupport@rgcjonas.gmail.com"
-        "dash-to-dock@micxgx.gmail.com"
-        "forge@jmmaranan.com"
-	"tailscale-status@maxgallup.github.com"
-      ];
-    };
-  };
-
   programs = {
     librewolf.enable = true;
     neovim.enable = true;
