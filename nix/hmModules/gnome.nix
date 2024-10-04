5d31afe129c8403ddce0a25e33a8a264f95b10fc
schMarXman <asdf@test.com>
2024-10-04 19:28:17 +0200

    Add caffeine and settings; formatting

--- nix/hmModules/gnome.nix
+++ nix/hmModules/gnome.nix
@@ -12,27 +12,35 @@
             gnomeExtensions.dash-to-dock
             gnomeExtensions.forge
             gnomeExtensions.tailscale-status
+            gnomeExtensions.caffeine
         ];
 
         # Gnome settings
         dconf.settings = {
-        # Window buttons
-        "org/gnome/desktop/wm/preferences" = {
-            button-layout = "appmenu:minimize,close";
-        };
+            # Window buttons
+            "org/gnome/desktop/wm/preferences" = {
+                button-layout = "appmenu:minimize,close";
+            };
+            "org/gnome/mutter" = {
+                dynamic-workspaces = true;
+            };
+            "org/gnome/desktop/interface" = {
+                enable-hot-corners = false;
+            };
 
-        # Keybindings
-        "org/gnome/desktop/wm/keybindings" = {
-            minimize = ["<Super>x"];
-        };
+            # Keybindings
+            "org/gnome/desktop/wm/keybindings" = {
+                minimize = ["<Super>x"];
+            };
 
-        "org/gnome/shell" = {
-            disable-user-extensions = false;
-            enabled-extensions = [
-                "appindicatorsupport@rgcjonas.gmail.com"
-                "dash-to-dock@micxgx.gmail.com"
-                "forge@jmmaranan.com"
-                "tailscale-status@maxgallup.github.com"
+            "org/gnome/shell" = {
+                disable-user-extensions = false;
+                enabled-extensions = [
+                    "appindicatorsupport@rgcjonas.gmail.com"
+                        "dash-to-dock@micxgx.gmail.com"
+                        "forge@jmmaranan.com"
+                        "tailscale-status@maxgallup.github.com"
+                        "caffeine@patapon.info"
                 ];
             };
         };
