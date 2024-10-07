949e72f954182c9a656741249f418196890debe3
schMarXman <asdf@test.com>
2024-10-07 21:20:11 +0200

    Add nix-ld

--- nix/hosts/T495s/configuration.nix
+++ nix/hosts/T495s/configuration.nix
@@ -119,6 +119,12 @@
 
   services.tailscale.enable = true;
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = [
+    # nvim nim lsp deps e.g.
+    pkgs.pcre
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
